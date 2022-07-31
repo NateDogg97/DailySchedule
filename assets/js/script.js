30efe4dccdf681003d56d1211006003f9d23ed7c
NateDogg <natmays97@gmail.com>
2022-07-31 17:34:53 -0500

    added time-blocks and bootstrap styling'
    git push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,6 @@
 var currentDay = $('#currentDay');
 
-// var today = new Date();
-// var dd = String(today.getDate()).padStart(2, '0');
-// var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-// var yyyy = today.getFullYear();
-
-// today = mm + '/' + dd + '/' + yyyy;
-
 var today = moment().format('MMMM Do YYYY')
 
 currentDay.text('Today is: ' + today);
+
